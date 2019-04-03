d45f5b825962ff1414d2b0fe30668305e8b55d0d
Devarsh shukla <Devarsh14@gmail.com>
2019-04-03 20:58:40 +1000

    Console and web application created.

---
+++ AzureCosmosConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureCosmosConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
